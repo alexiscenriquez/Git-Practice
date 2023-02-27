11a282bd5ea90d6a14b9b8c1acb373d12713bef0
waaade <tomtwelve@gmail.com>
2023-02-27 14:36:09 -0700

    add helloworld

--- src/com/coginixia/jump/gitpractice/HelloWorld.java
+++
@@ -1,5 +0,0 @@
-package com.coginixia.jump.gitpractice;
-
-public class HelloWorld {
-
-}
---
+++ src/com/coginixia/jump/gitpractice/com/coginixia/jump/gitpractice/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.coginixia.jump.gitpractice;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+}
